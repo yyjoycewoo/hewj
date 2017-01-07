a06f07e7cba0d79b25e20bc15121a18fe42d9c71
Joyce Woo <yyjoycewoo@gmail.com>
2017-01-07 16:50:01 -0500

    Added list of names to AttendanceActivity

--- MyApplication/app/src/main/java/com/example/android/classroom/AttendanceActivity.java
+++ MyApplication/app/src/main/java/com/example/android/classroom/AttendanceActivity.java
@@ -1,30 +1,61 @@
 package com.example.android.classroom;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class AttendanceActivity extends AppCompatActivity {
+
+    private TextView mStudentNamesTextView;
+    private LinearLayout attendanceLinearLayout;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance);
-    /*    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        attendanceLinearLayout = (LinearLayout) findViewById(R.id.attendance_list);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    */
+        String[] studentNames = new String[]{
+                "Joyce",
+                "Red Toy Wagon",
+                "Chemistry Set",
+                "Yo-Yo",
+                "Pop-Up Book",
+                "Generic Uncopyrighted Mouse",
+                "Finger Paint",
+                "Sock Monkey",
+                "Microscope Set",
+                "Beach Ball",
+                "BB Gun",
+                "Green Soldiers",
+                "Bubbles",
+                "Spring Toy",
+                "Fortune Telling Ball",
+                "Plastic Connecting Blocks",
+                "Water Balloon",
+        };
+
+        for (String name : studentNames) {
+            TextView student = new TextView(this);
+            student.setText(name);
+            student.setLayoutParams(new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT));
+
+            ((LinearLayout) attendanceLinearLayout).addView(student);
+        }
+
+        attendanceLinearLayout.getChildAt(2).setBackgroundColor(0xFF00FF00);
+
     }
-
 }
+
