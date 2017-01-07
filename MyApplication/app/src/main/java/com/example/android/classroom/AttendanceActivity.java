c37aa08959956c82d22abbd85d4b7e466c297124
Joyce Woo <yyjoycewoo@gmail.com>
2017-01-07 17:39:16 -0500

    Created class of fake student names

--- MyApplication/app/src/main/java/com/example/android/classroom/AttendanceActivity.java
+++ MyApplication/app/src/main/java/com/example/android/classroom/AttendanceActivity.java
@@ -15,30 +15,12 @@ public class AttendanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_attendance);
         attendanceLinearLayout = (LinearLayout) findViewById(R.id.attendance_list);
 
-        String[] studentNames = new String[]{
-                "Joyce",
-                "Red Toy Wagon",
-                "Chemistry Set",
-                "Yo-Yo",
-                "Pop-Up Book",
-                "Generic Uncopyrighted Mouse",
-                "Finger Paint",
-                "Sock Monkey",
-                "Microscope Set",
-                "Beach Ball",
-                "BB Gun",
-                "Green Soldiers",
-                "Bubbles",
-                "Spring Toy",
-                "Fortune Telling Ball",
-                "Plastic Connecting Blocks",
-                "Water Balloon",
-        };
+        String[] studentNames = FakeStudentNames.getStudentNames();
 
         for (String name : studentNames) {
             TextView student = new TextView(this);
             student.setText(name);
-            student.setTextSize(getResources().getDimensionPixelSize(R.dimen.tv_student_name));
+            student.setTextSize(getResources().getDimensionPixelSize(R.dimen.tv_textsize));
             student.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT));
---
+++ MyApplication/app/src/main/java/com/example/android/classroom/FakeStudentNames.java
@@ -0,0 +1,27 @@
+package com.example.android.classroom;
+
+/**
+ * Created by JoyceWoo on 2017-01-07.
+ */
+
+final class FakeStudentNames {
+    public static String[] getStudentNames() {
+        return new String[] {
+            "Joyce",
+            "David",
+            "Hinson",
+            "Eduoard",
+            "Will",
+            "Joyce",
+            "David",
+            "Hinson",
+            "Eduoard",
+            "Will",
+            "Joyce",
+            "David",
+            "Hinson",
+            "Eduoard",
+            "Will"
+        };
+    }
+}
