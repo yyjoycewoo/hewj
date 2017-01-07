8248b10f79d855547fe7a6c1d6fcd54877c9f506
Joyce Woo <yyjoycewoo@gmail.com>
2017-01-07 15:05:06 -0500

    Added activities for Attendance, Question, and Completion

--- MyApplication/app/src/main/java/com/example/android/classroom/MenuActivity.java
+++ MyApplication/app/src/main/java/com/example/android/classroom/MenuActivity.java
@@ -1,31 +1,64 @@
 package com.example.android.classroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.R.attr.type;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button mAttendanceButton;
+    private Button mQuestionButton;
+    private Button mCompletionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-    /*    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mAttendanceButton = (Button) findViewById(R.id.attendance_button);
+        mAttendanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goTo("attendance");
             }
         });
-    */
+
+        mQuestionButton = (Button) findViewById(R.id.question_button);
+        mQuestionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goTo("question");
+            }
+        });
+
+        mCompletionButton = (Button) findViewById(R.id.completion_button);
+        mCompletionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goTo("completion");
+            }
+        });
     }
 
+    public void goTo(String activity) {
+        Intent intent = null;
 
+        if (activity.equalsIgnoreCase("attendance")) {
+            intent = new Intent(this, AttendanceActivity.class);
+        } else if (activity.equalsIgnoreCase("question")) {
+            intent = new Intent(this, QuestionActivity.class);
+        } else if (activity.equalsIgnoreCase("completion")) {
+            intent = new Intent(this, CompletionActivity.class);
+        }
+
+        startActivity(intent);
+    }
 }
