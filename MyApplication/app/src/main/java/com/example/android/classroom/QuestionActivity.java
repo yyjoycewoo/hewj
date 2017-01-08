4c4256a12814549313c4ad4821334e8806de6c8e
William Kim <scarf3blue@gmail.com>
2017-01-07 21:30:50 -0500

    Add NewQuestionActivity

--- MyApplication/app/src/main/java/com/example/android/classroom/QuestionActivity.java
+++ MyApplication/app/src/main/java/com/example/android/classroom/QuestionActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.classroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -11,25 +14,25 @@ import java.util.List;
 public class QuestionActivity extends AppCompatActivity {
 
     private LinearLayout mQuestionList;
-    private List<String> mQuestions = null;
+    private Button mNewQuestionButton;
+    private List<String> mQuestions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mQuestionList = (LinearLayout) findViewById(R.id.question_list);
+
+        mNewQuestionButton = (Button) findViewById(R.id.new_question);
+        mNewQuestionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                goToNewQuestion();
             }
-        });*/
+        });
 
-        mQuestionList = (LinearLayout) findViewById(R.id.question_list);
         mQuestions = new ArrayList<>();
-
         populateQuestions(mQuestions);
 
         String questionText = "%s. %s";
@@ -38,7 +41,7 @@ public class QuestionActivity extends AppCompatActivity {
         for (i = 0; i < mQuestions.size(); i++) {
             TextView student = new TextView(this);
             student.setText(String.format(questionText, i + 1, mQuestions.get(i)));
-            student.setTextSize(getResources().getDimensionPixelSize(R.dimen.tv_student_name));
+            student.setTextSize(getResources().getDimensionPixelSize(R.dimen.tv_textsize));
             student.setLayoutParams(new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT));
@@ -49,7 +52,22 @@ public class QuestionActivity extends AppCompatActivity {
         //mQuestionList.getChildAt(1).setBackgroundColor(0xFF00FF00);
     }
 
+    private void goToNewQuestion() {
+        Intent intent = new Intent(this, NewQuestionActivity.class);
+        startActivity(intent);
+    }
+
     private void populateQuestions(List array) {
+        array.add("hi");
+        array.add("world");
+        array.add("hi");
+        array.add("world");
+        array.add("hi");
+        array.add("world");
+        array.add("hi");
+        array.add("world");
+        array.add("hi");
+        array.add("world");
         array.add("hi");
         array.add("world");
         array.add("hi");
